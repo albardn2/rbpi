60810a028ce0edb86d7ce116dd21fb4e9e322731
zaid <zaid.bardan@gmail.com>
2019-04-07 16:57:04 -0500

    try

--- led/views.py
+++ led/views.py
@@ -14,16 +14,16 @@ def home(request):
     return render(request, 'home.html')
 
 def turnon(request):
-    # factory = PiGPIOFactory(host='127.0.0.1')
-    led = LED(17)
+    factory = PiGPIOFactory(host='127.0.0.1')
+    led = LED(17, pin_factory=factory)
     led.on()
     print('ONNNN')
     return redirect('home')
 
 
 def turnoff(request):
-    # factory = PiGPIOFactory(host='127.0.0.1')
-    led = LED(17)
+    factory = PiGPIOFactory(host='127.0.0.1')
+    led = LED(17, pin_factory=factory)
     led.off()
     print('OFFFF')
     return redirect('home')
